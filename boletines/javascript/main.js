d888e6ee93fd9216fde1623179b3d165ef195bb7
eliasguzman <66865659+Elias-Guzman@users.noreply.github.com>
2020-06-14 19:24:09 -0500

    Subido por primera vez

---
+++ boletines/javascript/main.js
@@ -0,0 +1,32 @@
+// Obtener los elementos de la clase .clos//
+
+let links = document.querySelectorAll(".close");
+
+// Recorrerlos
+
+lincks.forEach(function(link){
+
+    //Agregar un evento click a cada uno de ellos
+      link.addEventListener("click",function(){
+
+      })
+})
+// DOM
+//querySelector////querySalectorAll//
+/*
+let links = document.querySelectorAll("a")
+
+links.forEach(funtion(link){
+  console.log(link;)
+
+});
+*/
+
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+  td.addEventListener('click',function(){
+    console.log(this)
+  })
+});
+*/
